febb4b59752d46cbe935f09cc0b317f66c40699d
AlexNik13 <4pnav21@gmail.com>
2021-11-22 17:00:00 +0200

    Start work customer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
+import ingredient.IngredientPizza;
 import product.pizza.Pizza;
+import product.pizza.SizePizza;
 
 public class Main {
     public static void main(String[] args) {
-        Pizza pizza = new Pizza("Meat",100, 150, "Meat/chasse");
+        Pizza pizza = new Pizza("Meat", SizePizza.XL, 150, 250,"Meat/chasse");
 
-        System.out.println(pizza.toString());
-        pizza.printFromMenu();
+        System.out.println(pizza.getCost());
+        pizza.addIngredientPizza(new IngredientPizza("Cыр пармезан",
+                50,
+                50));
+
+        System.out.println(pizza.getCost());
+
     }
 }
